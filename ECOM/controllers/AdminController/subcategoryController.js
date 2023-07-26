587a285f2ceb142f0d79cc54640f2b95f63b53c8
dhruvilmiyani18 <dhruvilmiyani300@gmail.com>
2023-07-26 16:35:22 +0530

    ecom delete

--- ECOM/controllers/AdminController/subcategoryController.js
+++
@@ -1,8 +0,0 @@
-const Category = require('../../models/Category');
-
-module.exports.add_subcategory = async (req,res) =>{
-    let catData = await Category.find({});
-    return res.render("Add_subcategory",{
-        catdata : catData
-    });
-}
